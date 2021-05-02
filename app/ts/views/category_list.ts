f3499d7d244acb5223ade6da82cd080c25c4a0a3
David Edson <david.contato.tec@gmail.com>
2021-05-02 00:21:25 -0300

    FEAT: master - theme change for theme dark and create methods for delete and search category

--- app/ts/views/category_list.ts
+++ app/ts/views/category_list.ts
@@ -17,20 +17,108 @@ const ajaxRequestGetCategories = () => {
     })
 }
 
+const ajaxRequestDeleteCategory = (id: number) => {
+    $.ajax({
+        url: `http://localhost:3100/category/${id}`,
+        method: 'DELETE',
+        dataType: 'json',
+        success: function(data){
+            if(data.success){
+                deleteCardCategory(id)
+                showMessage('success', data.message)
+                return
+            }
+            console.log(data)
+        },
+        error: function(er){
+            console.log(er)
+        }
+    })
+}
+
+const ajaxRequestSearchCategory = (name: string) => {
+    $.ajax({
+        url: `http://localhost:3100/category/${name}`,
+        method: 'GET',
+        dataType: 'json',
+        success: function(data){
+            if(data.success){
+                createCardsCategories(data.categories)
+                return
+            }
+            console.log(data)
+        },
+        error: function(er){
+            console.log(er)
+        }
+    })
+}
+
 const createCardsCategories = (categories: Category[]) => {
     let divCategories = $('#categories')
+    divCategories.html('')
 
     categories.forEach((item) => {
         divCategories.append(`
-        <div class="card">
-            <div class="card-body">
-                <div>
-                    <label for="category">Categoria:</label>
-                    <input type="text" class="form-control" value="${item.name}" readonly/>
-                    <button class="btn btn-warning mt-3">Editar</button>
-                </div>
-            </div>
-        </div>
+            <tr id="${item.id}">
+                <td>${item.name}</td>
+                <td>
+                    <button class="btn btn-info btn-sm">Editar</button>
+                </td>
+                <td>
+                    <button class="btn btn-danger btn-sm" onclick="deleteCategory(${item.id})">X</button>
+                </td>
+            </tr>
         `)
     })
+}
+
+const deleteCardCategory = (id: number) => {
+    let divCategory = $('#categories').find(`#${id}`)
+    divCategory.remove()
+}
+
+$('#btn-category-search').on('click', () => {
+    
+    const categoryField = $('#category-search-field').val()
+
+    if(categoryField === '' || typeof categoryField === undefined) {
+        showMessage('warning', 'Campo de busca de categoria deve ser preenchido.')
+        return
+    }
+
+    searchByCategory(categoryField.toString())
+})
+
+const showMessage = (status:string, msg: string) => {
+    
+    let messageElement = $('.message')
+    
+    messageElement.addClass(`alert alert-${status}`)
+    messageElement.text(msg)
+    messageElement.fadeIn()
+    
+    setTimeout(()=>{
+        messageElement.fadeOut()
+    }, 4000)
+}
+
+const deleteCategory = (id: number) => {
+    
+    if(typeof id == undefined){ 
+        showMessage('danger', 'parametro ID da categoria não encontrado.')
+        return
+    }
+    
+    ajaxRequestDeleteCategory(id)
+}
+
+const searchByCategory = (name: string) => {
+    
+    if(name == '' || typeof name == undefined){ 
+        showMessage('danger', 'Campo de categoria precisa ser preenchido para que a busca seja realizada.')
+        return
+    }
+
+    ajaxRequestSearchCategory(name)
 }
