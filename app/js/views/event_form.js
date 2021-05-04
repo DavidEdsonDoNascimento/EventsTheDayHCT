ba2b5ac8e9a93a172799a7477c3b02db059541b5
David Edson <david.contato.tec@gmail.com>
2021-05-03 23:15:30 -0300

    FEAT: master - created ajax that saves the event and method that clears the event entries

--- app/js/views/event_form.js
+++ app/js/views/event_form.js
@@ -22,3 +22,35 @@ const popularTheElement = (element, items) => {
         element.append(`<option value="${category.id}">${category.name}</option>`);
     });
 };
+$('#btn-save-event').on('click', () => {
+    let summary = $('#summary').val();
+    let obs = $('#obs').val();
+    let selectedCategory = $('#select-category').val();
+    if (selectedCategory == 0) {
+        return;
+    }
+    $.ajax({
+        url: `http://localhost:3100/event`,
+        method: 'POST',
+        dataType: 'json',
+        data: {
+            summary: summary,
+            obs: obs,
+            category_id: selectedCategory
+        },
+        success: function (data) {
+            if (data.success) {
+                showMessage('success', data.message);
+                resetEventInputs();
+            }
+        },
+        error: function (er) {
+            console.log(er);
+        }
+    });
+});
+const resetEventInputs = () => {
+    $('#summary').val('');
+    $('#obs').val('');
+    $('#select-category').val('0');
+};
--- app/ts/views/event_form.ts
+++ app/ts/views/event_form.ts
@@ -32,3 +32,42 @@ const popularTheElement = (element: JQuery, items: Category[]) => {
     })
 
 }
+
+$('#btn-save-event').on('click', () => {
+    
+    let summary = $('#summary').val()
+    let obs = $('#obs').val()
+    let selectedCategory = $('#select-category').val()
+
+    if(selectedCategory == 0){
+        return
+    }
+
+    $.ajax({
+        url: `http://localhost:3100/event`,
+        method: 'POST',
+        dataType: 'json',
+        data: {
+            summary: summary,
+            obs: obs,
+            category_id: selectedCategory
+        },
+        success: function(data){
+            
+            if(data.success){
+                showMessage('success', data.message)
+                resetEventInputs()
+            }
+        },
+        error: function(er){
+            console.log(er)
+        }
+    })
+
+})
+
+const resetEventInputs = () => {
+    $('#summary').val('')
+    $('#obs').val('')
+    $('#select-category').val('0')
+}
