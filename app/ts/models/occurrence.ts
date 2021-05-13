20230ec37e6ee6fd90010dcf00d8569b96c3ad8a
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2021-05-12 21:53:29 -0300

    FIX: master - listing occurrences and layout adjustment

--- app/ts/models/occurrence.ts
+++ app/ts/models/occurrence.ts
@@ -1,6 +1,7 @@
 class Occurrence {
     
     private _id: string
+    private _create_in: string
     
     constructor(private _summary: string, private _obs: string, private _category_id: string, private _category_name: string){}
 
@@ -9,5 +10,5 @@ class Occurrence {
     get obs() { return this._obs; }
     get category_id() { return this._category_id; }
     get category_name() { return this._category_name; }
-
+    get create_in() { return this._create_in; }
 }
--- app/ts/views/occurrence_list.ts
+++ app/ts/views/occurrence_list.ts
@@ -14,7 +14,7 @@ const loadsTheOccurrences = () => {
                 return
             }
             
-            popularTheElement($('#occurrence-list-day'), data.occurrence)
+            popularTheElement($('#occurrence-list-day-body'), data.occurrence)
         },
         error: function(er){
             console.log(er)
@@ -26,61 +26,15 @@ const popularTheElement = (element: JQuery, occurrences: Occurrence[]) => {
     occurrences.forEach(item => {
         element.append(templateUpdate(item))
     })
-
 }
 
 const templateUpdate = (model: Occurrence) => {
     return `
-    <div class="card col-sm-12">
-        <div class="card-title">
-            <div class="row">
-                <div class="col-sm-4">
-                    <input type="checkbox" name="task-focus" id="task-focus" onclick="newTime('teste')">
-                    <label for="task-focus">
-                        Focar nessa tarefa
-                    </label>
-                </div>
-                <div class="col-sm-8">
-                    <span style="font-size:12px; color: #9667E9">21 de Abril de 2021</span>
-                </div>
-            </div>
-        </div>
-        <div class="card-body">
-            <span style="font-size: bold">Resumo:</span>
-            <p>
-                ${model.summary}
-            </p>
-            
-            <button class="btn btn-sm btn-warning" data-toggle="collapse" data-target="#schedule">Horarios</button>
-            
-            <div id="schedule" class="collapse border p-2 mt-3">
-
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="times">
-                            08:15 até 08:29 
-                        </div>
-                    </div>
-                    <div class="col-sm-8">
-                        <button class="btn btn-sm btn-default">Editar</button>
-                        <button class="btn btn-sm btn-danger" style="border-radius:50%" onclick="removeTime(1)">X</button>
-                    </div>
-                </div>
-                
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="times">
-                            09:10 até 09:50
-                        </div>
-                    </div>
-                    <div class="col-sm-8">
-                        <button class="btn btn-sm btn-default">Editar</button>
-                        <button class="btn btn-sm btn-danger" style="border-radius:50%" onclick="removeTime(2)">X</button>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
+        <tr>
+            <td>${model.summary}</td>
+            <td>${model.create_in}</td>
+            <td><button class="btn btn-sm btn-warning">Horários</button></td>
+            <td><button class="btn btn-sm btn-primary">Detalhes</button></td>
+        </tr>
     `
 }
