2a0ee9ce538144c5a3c1859abfccdb27f5c133e2
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2021-05-15 23:31:10 -0300

    FEAT: master - adjusted frontend to display a button to edit the occurrence json in the occurrences list

--- app/ts/views/occurrence_list.ts
+++ app/ts/views/occurrence_list.ts
@@ -35,6 +35,60 @@ const templateUpdate = (model: Occurrence) => {
             <td>${model.create_in}</td>
             <td><button class="btn btn-sm btn-warning">Horários</button></td>
             <td><button class="btn btn-sm btn-primary">Detalhes</button></td>
+            <td>
+                <button class="btn btn-sm btn-secundary" data-toggle="modal" onclick="openJsonEditModal(${model.id})">
+                JSON
+                </button>
+            </td>
         </tr>
     `
+}
+
+const openJsonEditModal = (occurrenceId: string) => {
+    $.ajax({
+        url: `http://localhost:3100/occurrence/${occurrenceId}`,
+        method: 'GET',
+        dataType: 'json',
+        success: function(data){
+            
+            if(!data.success){
+                console.log(data)
+                return
+            }
+            
+            const { result } = data
+
+            if(result.length <= 0){
+                return;
+            }
+            
+            const occurrenceObject = $(result).get(0)
+            const occurrenceJson = JSON.stringify(occurrenceObject, null, '\t')
+
+            let modalJsonEdit = $('#modal-json-edit')
+            let jsonContentModal = modalJsonEdit.find('#json-content')
+            let btnSaveJson = modalJsonEdit.find('#btn-json-save')
+
+            jsonContentModal.val(occurrenceJson)
+            btnSaveJson.attr('onclick', `saveJsonOccurrence(${occurrenceObject.id})`)
+            modalJsonEdit.show('slow')
+
+        },
+        error: function(er){
+            console.log(er)
+        }
+    })
+}
+
+$('#btn-close-modal').on('click', () => {
+    clearJsonEditModal()
+    $('#modal-json-edit').hide('slow')
+})
+
+const clearJsonEditModal = () => {
+    $('#son-content').val('')
+}
+
+const saveJsonOccurrence = (occurrenceId: string) => {
+    alert('Está funcionalidade ainda está em contrução ...')
 }
