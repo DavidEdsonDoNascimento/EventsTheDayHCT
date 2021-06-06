fccb235fb0813b8d92b5e71a38f04a43946f8a7c
David Edson <david.contato.tec@gmail.com>
2021-06-06 19:27:39 -0300

    FEAT: master - placed new theme for category form and occurrence list

--- app/ts/views/occurrence_list.ts
+++ app/ts/views/occurrence_list.ts
@@ -1,7 +1,46 @@
+
 $(document).ready(function(){
 
-    loadsTheOccurrences()
-
+    //loadsTheOccurrences()
+    $('#occurrence-table').DataTable({
+        responsive: true,
+        ajax: {
+            url: 'http://localhost:3100/occurrences',
+            method: 'GET',
+            dataSrc: ''
+        },
+        columns: [
+            { data: 'summary' },
+            { 
+                data: 'createdAt',
+                render: (data, type) => {
+                    return Formatters.formatUTCDateStringToLocal(data)
+                } 
+            },
+            {
+                data: 'id',
+                render: (data, type) => {
+                    return `<button class="btn btn-sm btn-warning" onclick="openTimesModal(${data})">
+                                Horários
+                            </button>`
+                }
+            },
+            { 
+                data: 'id',
+                render: (data, type) => {
+                    return `<button class="btn btn-sm btn-primary">Detalhes</button>`
+                }
+            },
+            {
+                data: 'id',
+                render: (data, type) => {
+                    return `<button class="btn btn-sm btn-secundary" data-toggle="modal" onclick="openJsonEditModal(${data})">
+                                JSON
+                            </button>`
+                }
+            }
+        ]
+    })
 })
 
 const loadsTheOccurrences = () => {
@@ -56,33 +95,46 @@ const openTimesModal = (occurrenceId: string) => {
             let modalTimes = $('#modal-times')
             let ul = $('#ul-times')
 
-            timesJson.forEach(item => {
-                ul.append(
-                `<li id="time-${item.id}">
-                    <div class="row">
-                        <input type="text" class="form-control col-sm-3 fild-time" value="${Formatters.formatUTCDateStringToLocal(item.start)}" disabled/> 
-                        <span class="col-sm-2 text-center">até</span>
-                        <input type="text" class="form-control col-sm-3 fild-time" value="${Formatters.formatUTCDateStringToLocal(item.end)}" disabled/>
-                        
-                        <div class="btn-group btn-group-toggle col-sm-4" data-toggle="buttons">
-                            <button class="btn btn-secondary  btn-round active" onclick="disabledFildsTime(${item.id})">
-                                <input type="radio" name="options" id="option1" autocomplete="off" checked> T
-                            </button>
-                            <button class="btn btn-warning btn-round" onclick="enabledFildsTime(${item.id})">
-                                <input type="radio" name="options" id="option2" autocomplete="off"> D
-                            </button>
-                            <button class="btn btn-danger btn-round" onclick="removeTime(${occurrenceId}, ${item.id})">
-                                <input type="radio" name="options" id="option3" autocomplete="off"> X
-                            </button>
-                            <button class="btn btn-success btn-round" onclick="updateTime(${item.id})">
-                                <input type="radio" name="options" id="option3" autocomplete="off"> S
-                            </button>
+            if(timesJson.length == 0 ){
+                
+                ul.append(`<p class="text-center">Nenhum registro de tempo encontrado.</p>`)
+            }
+            else{
+
+                timesJson.forEach(item => {
+                    ul.append(
+                    `<li id="time-${item.id}" class="p-3" style="list-style-type: none">
+                        <div class="row">
+                            <input type="text" class="form-control col-sm-3 fild-time" value="${Formatters.formatUTCDateStringToLocal(item.start)}" disabled/> 
+                            <span class="col-sm-2 text-center">até</span>
+                            <input type="text" class="form-control col-sm-3 fild-time" value="${Formatters.formatUTCDateStringToLocal(item.end)}" disabled/>
+                            
+                            <div class="btn-group btn-group-toggle col-sm-4" data-toggle="buttons">
+                                <button class="btn btn-secondary  btn-round active" onclick="disabledFildsTime(${item.id})">
+                                    <input type="radio" name="options" id="option1" autocomplete="off" checked>
+                                    <i class="fas fa-lock"></i>
+                                </button>
+                                <button class="btn btn-warning btn-round" onclick="enabledFildsTime(${item.id})">
+                                    <input type="radio" name="options" id="option2" autocomplete="off">
+                                    <i class="fas fa-lock-open"></i>
+                                </button>
+                                <button class="btn btn-danger btn-round" onclick="removeTime(${occurrenceId}, ${item.id})">
+                                    <input type="radio" name="options" id="option3" autocomplete="off">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                                <button class="btn btn-success btn-round" onclick="updateTime(${item.id})">
+                                    <input type="radio" name="options" id="option3" autocomplete="off"> 
+                                    <i class="fas fa-check"></i>
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                </li>`
-                )
-            })
-            modalTimes.show('slow')
+                    </li>`
+                    )
+                })
+            }
+
+            
+            modalTimes.show()
 
         },
         error: function(er){
